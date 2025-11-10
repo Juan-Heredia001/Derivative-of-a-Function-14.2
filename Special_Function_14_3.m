810de8cf0273153329b4377906301f329c5fb9bc
John <jiheredia1@my.scccd.edu>
2025-11-10 22:06:59 +0000

    Last Lab practice for Monday Week 14

--- Special_Function_14_3.m
+++ Special_Function_14_3.m
@@ -1,1 +1,16 @@
+% Integral of a Polynomial
+p = input('Enter the coefficients of a cubic polynomial, as a matrix [a b c d].\n');
+y0 = input('Enter the integration constant for that function.\n');
+
+% Find the coefficients of the polynomial that is the derivative of
+% original polynomial
+pDer = polyder(p);
+
+% Find coefficients of the polynomial that is the integral of original polynomial.
+pint = polyint(p,y0);
+
+% Report the output
+fprintf("The derivative of the function is %gx^2+%gx+%g.\n",pDer)
+fprintf("The integral of the function is" + ...
+    " %gx^4+%gx^3+%.gx^2+%gx+%.2f.\n",pint)
 
